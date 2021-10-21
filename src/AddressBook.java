656ca257f1dda24be44d9461058ad6b4c36cc3d2
MahtabAmeli <Adminjan93>
2021-10-21 05:19:41 -0400

    added Main to class

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+
+    private BuddyInfo buddy1 = new BuddyInfo("b1", "a1", "n1");
+    private BuddyInfo buddy2 = new BuddyInfo("b2", "a2", "n2");
+    private BuddyInfo buddy3 = new BuddyInfo("b3", "a3", "n3");
+    private BuddyInfo buddy4 = new BuddyInfo("b4", "a4", "n4");
+
+    public ArrayList<BuddyInfo> buddyList = new ArrayList();
+
+    public AddressBook() {
+        this.buddyList.add(buddy1);
+        this.buddyList.add(buddy2);
+        this.buddyList.add(buddy3);
+        this.buddyList.add(buddy4);
+    }
+
+
+    public void addBuddy(BuddyInfo buddy) {
+        buddyList.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddyList.remove(buddy);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
